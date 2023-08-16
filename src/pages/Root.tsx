bdbf7664d671233bca79deac986dcfc44f05a83e
Shuvam <shubhamduttadnk@gmail.com>
2023-08-16 11:25:31 +0530

    scroll to top fixed

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,19 @@
 import React, { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import MainNavigationBar from '../components/MainNavigation/MainNavigationBar';
 import Footer from '../components/Footer/Footer';
 const Root: React.FC = () => {
+    const {pathname} = useLocation();
+    console.log(pathname);
+
+    useEffect(() => {
+        window.scrollTo({
+            top:0,
+            left:0,
+            behavior:'instant'
+        });
+    },[pathname])
+    
     return (
         <>
             <MainNavigationBar />
