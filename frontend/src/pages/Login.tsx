19661de6ab3876ee91009a4dae21006457748f82
Shuvam <shubhamduttadnk@gmail.com>
2024-01-16 14:33:15 +0530

    login redirect updated

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,34 @@
 import React, {useEffect} from 'react';
 import LoginForm from '../components/LoginPageComponents/LoginForm/LoginForm';
-
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { MoonLoader } from "react-spinners";
 const Login:React.FC = () => {
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    const navigate = useNavigate();
     useEffect(() => {
         document.title = 'Login';
-    },[])
+        if(isAuthenticated){
+            navigate('/');
+        }
+    },[isAuthenticated])
     return (
         <>
-            <LoginForm />
+            {
+                !isAuthenticated && <LoginForm />
+            }
+
+            {
+                isAuthenticated && <div className='loader_overlay'>
+                <MoonLoader
+                    color={'#C86A83'}
+                    loading={isAuthenticated}
+                    size={80}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+            </div>
+            }
         </>
     );
 }
